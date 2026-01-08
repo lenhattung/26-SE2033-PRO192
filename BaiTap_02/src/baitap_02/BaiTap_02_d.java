5f201ad59c3c87e6247e34b4b4af3b6dea9660e8
lenhattung <lenhattung@gmail.com>
2026-01-08 13:47:57 +0700

    u
    u

---
+++ BaiTap_02/src/baitap_02/BaiTap_02_d.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package baitap_02;
+
+/**
+ *
+ * @author tungi
+ */
+public class BaiTap_02_d {
+    public static void main(String[] args) {
+        // Giai phuong trinh bac 2: ax^2 + bx + c = 0;
+        // a,b,c -> nhap tu ban phim (double)
+    }
+}
