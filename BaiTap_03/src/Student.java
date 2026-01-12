b7a36b0e471993ec2e9c109eba5a827545c67342
lenhattung <lenhattung@gmail.com>
2026-01-12 14:04:02 +0700

    u
    u

--- BaiTap_03/src/Student.java
+++ BaiTap_03/src/Student.java
@@ -8,10 +8,10 @@
  * @author tungi
  */
 public class Student {
-    public String id;
-    public String fullName;
-    public int yearOfBrith;
-    public double gpa;
+    private String id;
+    private String fullName;
+    private int yearOfBirth;
+    private double gpa;
 
     // default constructor 
     public Student() {
@@ -19,10 +19,14 @@ public class Student {
 
     // constructor with parameters
     public Student(String id, String fullName, int yearOfBrith, double gpa) {
-        this.id = id;
-        this.fullName = fullName;
-        this.yearOfBrith = yearOfBrith;
-        this.gpa = gpa;
+//        this.id = id;
+//        this.fullName = fullName;
+//        this.yearOfBirth = yearOfBrith;
+//        this.gpa = gpa;
+          setId(id);
+          setFullName(fullName);
+          setYearOfBirth(yearOfBirth);
+          setGpa(gpa);
     }
 
     
@@ -32,7 +36,45 @@ public class Student {
     
     @Override
     public String toString() {
-        return "Student{" + "id=" + id + ", fullName=" + fullName + ", yearOfBrith=" + yearOfBrith + ", gpa=" + gpa + ", Passed: "+ checkPass() + "}";
+        return "Student{" + "id=" + id + ", fullName=" + fullName + ", yearOfBrith=" + yearOfBirth + ", gpa=" + gpa + ", Passed: "+ checkPass() + "}";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        if (yearOfBirth<1)
+            this.yearOfBirth = 1;
+        else
+            this.yearOfBirth = yearOfBirth;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        if (gpa>=0 && gpa<=10)
+            this.gpa = gpa;
+        else
+            this.gpa =0;
     }
     
     
--- BaiTap_03/src/Tester.java
+++ BaiTap_03/src/Tester.java
@@ -15,7 +15,7 @@ public class Tester {
         Scanner sc = new Scanner(System.in);
 
         Student st1 = new Student("01", "Le Nhat Tung", 2006, 9.5);
-        
+
         System.out.print("Input n: ");
         int n = Integer.parseInt(sc.nextLine());
         Student[] sts = new Student[n];
@@ -31,7 +31,20 @@ public class Tester {
             double gpa = Double.parseDouble(sc.nextLine());
             sts[i] = new Student(id, fullName, yearOfBirth, gpa);
         }
-        
+
+//        for (int i = 0; i < n; i++) {
+//            sts[i] = new Student();
+//            System.out.println("Input student " + i + ": ");
+//            System.out.print("Id: ");
+//            sts[i].setId(sc.nextLine());
+//            System.out.print("Fullname: ");
+//            sts[i].setFullName(sc.nextLine());
+//            System.out.print("Year of birth: ");
+//            sts[i].setYearOfBirth(Integer.parseInt(sc.nextLine()));
+//            System.out.print("Gpa: ");
+//            sts[i].setGpa(Double.parseDouble(sc.nextLine()));
+//        }
+
         for (Student st : sts) {
             System.out.println(st);
         }
